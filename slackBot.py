6ed6d99803fa839d14f2b84b4ca76c798cc8ca1c
berkott <berkott3002@gmail.com>
2018-07-23 23:46:03 -0400

    adding exception handling for slackbot

--- slackBot.py
+++ slackBot.py
@@ -9,7 +9,7 @@ import csv
 # export SLACK_BOT_TOKEN='your bot user access token here'
 # slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
-
+slack_client = SlackClient('xoxb-397785387536-402566650723-MKUz9KcecNGBx7jqP8M3vSSt')
 
 ai_bot_id = None
 
@@ -50,9 +50,9 @@ def handle_command(command, channel, slack_logs):
     # [3] Games Played
     # [4] Start Time
 
+    print(slack_logs)
     currentTime = time.time()
     timeSinceStart = getTime(currentTime - slack_logs[4])
-    print(slack_logs)
 
     if command.startswith(COMMAND):
         response = "Generation: " + str(slack_logs[0]) + " Highest Score: " + str(slack_logs[1]) + " Current Score: " + str(slack_logs[2]) + " Games Played: " + str(slack_logs[3]) + " Time Since Start: " + str(timeSinceStart)
@@ -64,16 +64,42 @@ def handle_command(command, channel, slack_logs):
         text=response or default_response
     )
 
+def reconnectLoop(slack_client, channel, connectionLostTime):
+    connected=False
+    while not connected:
+        print("Attempting reconnection ...")
+        try:
+            connected = slack_client.rtm_connect(with_team_state=False)
+        except:
+            print("... failed")
+        time.sleep(10*RTM_READ_DELAY)
+    print("Reconnected")
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text="Connection lost at "+connectionLostTime+". Reconnected now."
+    )
+    
+    
+
 # if __name__ == "__main__":
 if slack_client.rtm_connect(with_team_state=False):
+    print(datetime.datetime.now())
     print("AI Bot connected and running!")
     # Read bot's user ID by calling Web API method `auth.test`
     ai_bot_id = slack_client.api_call("auth.test")["user_id"]
+    channelId="CBUFRK2EQ"
     while True:
-        command, channel = parse_bot_commands(slack_client.rtm_read())
-        if command:
-            slack_logs = getCsvData()
-            handle_command(command, channel, slack_logs)
-        time.sleep(RTM_READ_DELAY)
+        try:
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                slack_logs = getCsvData()
+                handle_command(command, channel, slack_logs)
+        except:
+            connectionLostTime = str(datetime.datetime.now())
+            print ("Connection failed at ", connectionLostTime)
+            reconnectLoop(slack_client, channelId, connectionLostTime)
+        finally:
+            time.sleep(RTM_READ_DELAY)
 else:
-    print("Connection failed. Exception traceback printed above.")
+    print("Connection failed from the start. Quitting...")
